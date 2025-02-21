5e39141fe1131769ea2961e89e7fe9cbc0b4d1fb
yuma-88 <ahiru1904@gmail.com>
2025-02-22 01:27:52 +0900

    modified:テストコードの修正

--- test/controllers/timers_controller_test.rb
+++ test/controllers/timers_controller_test.rb
@@ -1,16 +1,7 @@
 require "test_helper"
 
 class TimersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = User.create!(
-      email: "test_#{SecureRandom.uuid}@example.com",
-      password: "password",
-      password_confirmation: "password"
-    )
-  end
-
   test "should get show" do
-    sign_in @user  # Devise を使っている場合
     get timers_show_url
     assert_response :success
   end
