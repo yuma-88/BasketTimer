9f8b7451836001e6e9ebad4fd81a166142b0d81d
yuma-88 <ahiru1904@gmail.com>
2025-02-22 00:49:41 +0900

    modified:タイマー画面のルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "timers/show"
   devise_for :users
   resources :game_records, only: [ :index, :new, :create, :show, :edit, :update, :destroy ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   root "top#index"
 
-  get "/timers", to: "timers#index"
+  get "/timers", to: "timers#show"
   get "/game_records", to: "game_records#index"
   get "/rulegpts", to: "rulegpts#index"
   get "/settings", to: "settings#index"
