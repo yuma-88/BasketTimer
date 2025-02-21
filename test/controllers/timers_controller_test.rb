a1f750476ba697b170e056ae2677ca4b5318c021
yuma-88 <ahiru1904@gmail.com>
2025-02-22 01:22:37 +0900

    modified:テストコードの修正

--- test/controllers/timers_controller_test.rb
+++ test/controllers/timers_controller_test.rb
@@ -10,6 +10,7 @@ class TimersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get show" do
+    sign_in @user  # Devise を使っている場合
     get timers_show_url
     assert_response :success
   end
