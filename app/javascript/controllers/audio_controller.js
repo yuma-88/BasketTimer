0bc10a005756dc4823e43822c956efcd18d74c0d
yuma-88 <ahiru1904@gmail.com>
2025-03-24 19:05:30 +0900

    add:カウントダウン音声の追加

--- app/javascript/controllers/audio_controller.js
+++ app/javascript/controllers/audio_controller.js
@@ -7,6 +7,23 @@ export default class extends Controller {
     // 音声オブジェクトの作成（1回だけ作成）
     this.endSound = new Audio("/sounds/end.mp3");
     this.buzzerSound = new Audio("/sounds/buzzer.mp3");
+
+    // 各カウントダウン音声ファイルの作成
+    this.countdownSounds = {
+      60: new Audio("/sounds/countdown_60.mp3"),
+      30: new Audio("/sounds/countdown_30.mp3"),
+      15: new Audio("/sounds/countdown_15.mp3"),
+      9: new Audio("/sounds/countdown_10.mp3"),
+      8: new Audio("/sounds/countdown_9.mp3"),
+      7: new Audio("/sounds/countdown_8.mp3"),
+      6: new Audio("/sounds/countdown_7.mp3"),
+      5: new Audio("/sounds/countdown_6.mp3"),
+      4: new Audio("/sounds/countdown_5.mp3"),
+      3: new Audio("/sounds/countdown_4.mp3"),
+      2: new Audio("/sounds/countdown_3.mp3"),
+      1: new Audio("/sounds/countdown_2.mp3"),
+      0: new Audio("/sounds/countdown_1.mp3"),
+    };
   }
 
   // 音声を再生するメソッド
@@ -33,10 +50,16 @@ export default class extends Controller {
     }
   }
   
-
   playEndSound() {
     if (this.endSound) {
       this.endSound.play();
+    }
+  }
+
+  // カウントダウン音声を再生
+  playCountdownSound(seconds) {
+    if (this.countdownSounds[seconds]) {
+      this.countdownSounds[seconds].play();
     }
   }
 }
