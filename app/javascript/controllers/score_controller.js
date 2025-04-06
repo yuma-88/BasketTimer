e1ab0f8c61ae5d62e83905f6d00b1f974a790486
yuma-88 <ahiru1904@gmail.com>
2025-04-06 15:20:27 +0900

    modify:スコアの強調表示を修正

--- app/javascript/controllers/score_controller.js
+++ app/javascript/controllers/score_controller.js
@@ -56,6 +56,9 @@ export default class extends Controller {
       // チームBを強調表示（文字色変更）
       this.teamBScoreTarget.classList.add("text-yellow-200");
       this.teamAScoreTarget.classList.remove("text-yellow-200");
+
+      // 明示的にDOMの再描画を強制
+      this.element.classList.toggle('force-repaint');
     }
   }
 
