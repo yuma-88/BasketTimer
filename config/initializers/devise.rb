428d143e606c8e700aca895a2682fa0bbe1c6b84
yuma-88 <ahiru1904@gmail.com>
2025-04-20 22:05:00 +0900

    modified:テストでは無効

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,5 +312,10 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret], skip_jwt: true
+  if Rails.env.development? || Rails.env.production?
+    config.omniauth :google_oauth2,
+      Rails.application.credentials.google[:client_id],
+      Rails.application.credentials.google[:client_secret],
+      skip_jwt: true
+  end
 end
