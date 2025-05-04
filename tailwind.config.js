73af5530457c04f27f14a99bf2e661367dd25720
yuma-88 <ahiru1904@gmail.com>
2025-05-05 01:50:01 +0900

    modified:タイマー画面をスマホ横画面に対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/assets/stylesheets/**/*.css',
   ],
+  theme: {
+    extend: {
+      screens: {
+        'mobile-landscape': { 'raw': '(max-width: 767px) and (orientation: landscape)' },
+      },
+    },
+  },
   plugins: [require("daisyui")],
   daisyui: {
     darkTheme: false, // ダークモードをONにする場合は削除
