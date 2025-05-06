4db9128ed9080d63cc86270e06c6f9ead97d2ee8
yuma-88 <ahiru1904@gmail.com>
2025-05-06 16:30:04 +0900

    modified:lintチェック修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.hosts << 'b-timer.jp'
+  config.hosts << "b-timer.jp"
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
